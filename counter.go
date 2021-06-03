970534eeb98fd85430f71e7485384b966b5db8f8
Tom <37644300+tom-miseur@users.noreply.github.com>
2021-06-03 08:25:54 -0600

    Added named counters

--- counter.go
+++ counter.go
@@ -4,6 +4,7 @@
 package counter
 
 import (
+	"sync"
 	"sync/atomic"
 
 	"go.k6.io/k6/js/modules"
@@ -13,14 +14,38 @@ func init() {
 	modules.Register("k6/x/counter", New())
 }
 
-var realCounter int64
+var anonCounter int64
 
-type counter struct{}
+type namedCounter struct {
+	idx *int64
+}
+
+type counter struct {
+	named map[string]*namedCounter
+	mu    sync.RWMutex
+}
 
 func (c *counter) Up() int64 {
-	return atomic.AddInt64(&realCounter, 1)
+	return atomic.AddInt64(&anonCounter, 1)
+}
+
+func (c *counter) UpNamed(name string) int64 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	counter, ok := c.named[name]
+
+	if !ok {
+		counter = &namedCounter{
+			idx: new(int64),
+		}
+		c.named[name] = counter
+	}
+
+	return atomic.AddInt64(counter.idx, 1)
 }
 
 func New() *counter {
-	return &counter{}
+	return &counter{
+		named: make(map[string]*namedCounter),
+	}
 }
